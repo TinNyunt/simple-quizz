fb89ac5305721680eddc258f2383e7eedfb9dd71
TinNyunt <cageystudio@gmail.com>
2023-05-01 17:55:35 +0630

    quizz

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import Quizz from "./Data/Question.jsx";
+
+function App() {
+  const [CurrentQuestion, setcurrentQuestion] = useState(0);
+  const [ShowScore, setshowScore] = useState(false);
+  const [GoodChoice, setgoodChoice] = useState(0);
+  const [BadChoice, setbadChoice] = useState(0);
+
+  const clickhanlder = isCorrect => {
+    if (isCorrect == true) {
+      setgoodChoice(GoodChoice + 1);
+    }
+    if (isCorrect == false) {
+      setbadChoice(BadChoice + 1);
+    }
+    const nextQuest = CurrentQuestion + 1;
+    setTimeout(() => {
+      if (nextQuest < Quizz.length) {
+        setcurrentQuestion(nextQuest);
+      } else {
+        setshowScore(true);
+      }
+    }, 1000);
+  };
+
+  return (
+    <div className="Container">
+      <div className="holder">
+        {ShowScore ? (
+          <div style={{ textAlign: "center" }}>
+            <div className="title">
+              <p style={{ fontSize: 25 }}>One piece Myanmar Quizz</p>
+            </div>
+            <hr></hr>
+            <div style={{ marginTop: 17 }}>
+              <p style={{ color: "green", fontSize: 25 }}>Good Choice</p>
+              <p>
+                {GoodChoice} / {Quizz.length}
+              </p>
+              <p style={{ color: "red", fontSize: 25 }}>Bad Choice</p>
+              <p>
+                {BadChoice} / {Quizz.length}
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div>
+            {/** Title */}
+            <div className="title">
+              <p style={{ fontSize: 25 }}>One Piece Myanmar Quizz</p>
+            </div>
+            <hr style={{ height: 0.1 }}></hr>
+            <div style={{ marginTop: 10 }}>
+              {/** Question */}
+              <div>
+                <p>အမေး : {Quizz[CurrentQuestion].Question}</p>
+              </div>
+              {/** Images */}
+              <div>
+                <img className="Image" src={Quizz[CurrentQuestion].img} />
+              </div>
+              {/** Answer */}
+              <div className="Item">
+                {Quizz[CurrentQuestion].AnswerOptions?.map(quest => (
+                  <div
+                    className="Answer"
+                    style={{ backgroundColor: quest.color }}
+                    onClick={() => clickhanlder(quest.isCorrect)}
+                  >
+                    <p
+                      style={{
+                        fontSize: 20,
+                        justifyContent: "center",
+                        display: "flex",
+                        lineHeight: 2,
+                      }}
+                    >
+                      {quest.AnswerText}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Data/Question.jsx
@@ -0,0 +1,74 @@
+const Quizz = [
+  {
+    id: 1,
+    Question: "Robin ပထမဆုံး bounty",
+    img: "https://i.ytimg.com/vi/_MzuYtaGbPQ/maxresdefault.jpg",
+    AnswerOptions: [
+      { AnswerText: "Age 10", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "Age 8", isCorrect: true, color: "#F4A407" },
+      { AnswerText: "Age 6", isCorrect: false, color: "#66CDAA" },
+      { AnswerText: "Age 7", isCorrect: false, color: "#00008B" },
+    ],
+  },
+  {
+    id: 2,
+    Question: "Roger ship နာမည်",
+    img: "https://i0.wp.com/doublesama.com/wp-content/uploads/2022/02/One-Piece-Oro-Jackson-Ship-1.jpg?resize=640%2C360",
+    AnswerOptions: [
+      { AnswerText: "Flying Dutchman", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "Moby Dick", isCorrect: false, color: "#F4A407" },
+      { AnswerText: "The Oro Jackson", isCorrect: true, color: "#C07B7B" },
+      { AnswerText: "Shark Superb", isCorrect: false, color: "#F4A407" },
+    ],
+  },
+  {
+    id: 3,
+    Question: "Franky နာမည်အရင်း",
+    img: "https://i.pinimg.com/originals/4a/df/9c/4adf9c35cdec9958b2465afd78f5fd0a.jpg",
+    AnswerOptions: [
+      { AnswerText: "Cutty flam", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "Morgan", isCorrect: false, color: "#F4A407" },
+      { AnswerText: "Flamie", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "Jango", isCorrect: false, color: "#F4A407" },
+    ],
+  },
+  {
+    id: 4,
+    Question: "Zoro real father နာမည်",
+    img: "https://i.pinimg.com/originals/97/aa/46/97aa465303c91ac305bfec559fd16ab1.jpg",
+    AnswerOptions: [
+      { AnswerText: "Roronoa Dickzoro", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "Roronoa Pinzoro", isCorrect: false, color: "#F4A407" },
+      { AnswerText: "Roronoa Arashi", isCorrect: true, color: "#C07B7B" },
+      {
+        AnswerText: "Shimotsuki Koushirou",
+        isCorrect: false,
+        color: "#F4A407",
+      },
+    ],
+  },
+  {
+    id: 5,
+    Question: "NewGate အသက်ဘယ်လောက်မှာ သေတာလဲ",
+    img: "https://animesher.com/orig/1/121/1219/12194/animesher.com_white-beard-newgate-black-and-white-1219458.jpg",
+    AnswerOptions: [
+      { AnswerText: "82", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "78", isCorrect: false, color: "#F4A407" },
+      { AnswerText: "75", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "72", isCorrect: true, color: "#F4A407" },
+    ],
+  },
+  {
+    id: 6,
+    Question: "Luffy မွေးရပ်‌‌‌မြေ",
+    img: "https://staticg.sportskeeda.com/editor/2022/03/5da10-16461487373069.png",
+    AnswerOptions: [
+      { AnswerText: "Foosha Village", isCorrect: true, color: "#C07B7B" },
+      { AnswerText: "Shimotsuki Village", isCorrect: false, color: "#F4A407" },
+      { AnswerText: "Gecko Islands", isCorrect: false, color: "#C07B7B" },
+      { AnswerText: "Fusha Village", isCorrect: false, color: "#F4A407" },
+    ],
+  },
+];
+
+export default Quizz;
